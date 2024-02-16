991368cc7e0f4a74c2250e0a2389ff091c333b10
McKay Knell <mckay@devpipeline.com>
2024-02-16 16:34:46 -0700

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+//1
+// for (let i = 0; i < 11; i += 2) {
+//   console.log(i);
+// }
+
+//2
+// const arrayLetters = ["a", "b", "c", "d", "e", "f"];
+// const arrayNumbers = [1, 5];
+// const finalArray = [];
+
+// for (let item of arrayNumbers) {
+//   finalArray.push(arrayLetters[item]);
+// }
+
+// console.log(finalArray);
+
+//3
+// const numbers = 12345;
+// const emptyArray = [];
+
+// for (let item of String(numbers)) {
+//   emptyAray.push(item);
+// }
+// console.log(emptyArray);
+
+//4
+// const arrayOne = [1, 2, 3];
+// const arrayTwo = ["a", "b", "c", "d"];
+// const mainArray = [];
+
+// for (let num of arrayOne) {
+//   for (let letter of arrayTwo) {
+//     mainArray.push([num, letter]);
+//     mainArray.push([letter, num]);
+//   }
+// }
+// console.log(mainArray);
